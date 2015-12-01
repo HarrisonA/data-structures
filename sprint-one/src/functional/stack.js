28319675904087c1f4e53a77a92b1e8595c3705b
HarrisonA <radiov@gmail.com>
2015-12-01 11:29:51 -0800

    Stack function complete

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,28 @@
 var Stack = function(){
   var someInstance = {};
+  
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function(){
-  };
+    if (size > 0){
+      size--;
+      var returnValue = storage[size];
+      storage[size] = undefined;
+      return returnValue;
+    }
 
+  };
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
