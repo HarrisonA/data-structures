f64645d0265f7fd0c3c8d6e6e535c4ed34d9ddd6
HarrisonA <radiov@gmail.com>
2015-12-02 12:04:40 -0800

    prototypal queue and stack complete

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,47 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var returnQueue = Object.create(queueMethods);
+  returnQueue.storage = {};
+  returnQueue.totalElemsInQueue = 0;
+  return returnQueue;
 };
 
 var queueMethods = {};
+
+queueMethods.size = function () {
+  return this.totalElemsInQueue;
+};
+
+queueMethods.enqueue = function (val) {
+  this.storage[this.totalElemsInQueue] = val;
+  this.totalElemsInQueue++;
+};
+
+queueMethods.dequeue = function () {
+  if ( this.totalElemsInQueue > 0 ) {
+    // take the first thing zero-ith thing
+    var returnElem = this.storage[0];
+    // loop and shift
+    for(var i = 0; i < this.totalElemsInQueue; i++) {
+      this.storage[i] = this.storage[i+1];
+    }
+    // decrement
+    this.totalElemsInQueue--;
+    // clear 
+    this.storage[this.totalElemsInQueue] = undefined;
+    return returnElem;
+  }
+};
+
+
+
+//  at Context.<anonymous> (file:///Users/student/Desktop/2015-11-data-structures/sprint-one/spec/specs.js:114:15)
+
+
+
+   
+
+
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var returnStack = Object.create(stackMethods);
+  returnStack.storage = {};
+  returnStack.elemsInStack = 0;
+  return returnStack;
+};
+// create shared method container
+var stackMethods = {};
+// returns stack instance size.
+stackMethods.size = function () {
+  return this.elemsInStack;
+};
+// adds element to top of the stack.
+stackMethods.push = function (val) {
+  this.storage[this.elemsInStack] = val;
+  this.elemsInStack++;
 };
 
-var stackMethods = {};
+// returns top element in stack.
+// also clears that element from memory.
+
+stackMethods.pop = function () {
+  if (this.elemsInStack > 0) {
+    this.elemsInStack--;
+    var returnElem = this.storage[this.elemsInStack];
+    this.storage[this.elemsInStack] = undefined;
+    return returnElem;
+  }
+}
 
 
